3f0383456de1d40e7fe6e13b581a637194983f42
franec94 <franec94@gmail.com>
2020-02-03 14:48:35 +0000

    First commit from replt.it web-app

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ from utils.custom_logger import get_custom_logger
 from utils.setup_program import setup_program
 
 def main():
+  # Date at which main entry-point is invoked.
   start_time = datetime.datetime.now()
   a_main_date_str: str = datetime.datetime.strftime(start_time, '%Y%m%d_%H%M%S')
   
+  # Get command line args
   cmd_args_obj, _ = get_cmd_line_args()
+
+  # Get main logger
   main_logger: logging.Logger = \
     get_custom_logger(
       cmd_args_obj.logs_dir,
@@ -23,9 +27,11 @@ def main():
 
   main_logger.info(f'[*] Running main at {a_main_date_str}')
 
+  # setup program based on cmd args
   setup_program(cmd_args_obj, main_logger)
 
 
+  # log end main script
   end_time = datetime.datetime.now()
   main_logger.info(f'[*] Running main, done.')
   main_logger.info(f'[*] Elapsed {end_time - start_time} since the begging of script.')
